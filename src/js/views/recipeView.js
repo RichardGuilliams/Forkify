2e9721afeb6bdc62174dda22fc1109f52ba20524
RichardGuilliams <richardlg231@gmail.com>
2022-08-15 00:36:49 -0600

    Implimented Check For Integration

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // parcel2 requires any non script asset to have a url: at the beginning of the string
-import {Fraction} from 'fracty';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View{
     _parentElement = document.querySelector('.recipe');
@@ -115,7 +115,7 @@ class RecipeView extends View{
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                <div class="recipe__quantity">${ing.quantity ? numberToFraction(ing.quantity).toString() : ''}</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
